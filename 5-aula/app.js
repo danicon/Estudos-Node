167b0145d6b08ee1cf87a2c1d861f9ffaa170189
Daniel Alferes <dancon.alferes@gmail.com>
2021-12-08 11:03:18 -0300

    Como carregar pagina html

---
+++ 5-aula/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/src/index.html")
+});
+
+app.get("/sobre-empresa", function(req, res) {
+    res.sendFile(__dirname + "/src/sobre-empresa.html")
+})
+
+app.get("/blog", function(req, res) {
+    res.send("Página do blog")
+})
+
+app.get("/contato", function(req, res) {
+    res.send("Página de contato")
+})
+
+//localhost:8080
+app.listen(8080);
